d85c9db22af9ea68d27f0e4c8d0a6836d166befd
Gracilliano <gracillianowp@hotmail.com>
2021-11-18 15:57:23 -0300

    Parte final do backend. Não está validando as rotas por mudanças no pacote passport

--- tasksBackend/index.js
+++ tasksBackend/index.js
@@ -1,8 +1,10 @@
 const app = require('express')()
 const db = require('./config/db')
 const consign = require('consign')
+const passport = require('passport')
 
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api')
     .then('./config/routes.js')
@@ -10,10 +12,7 @@ consign()
 //USANDO O INTO  FAZ COM PASSE O APP PARA TODOS OS MODULOS UTILIZADOS NA APLICAÇÃO
 
 app.db = db
-
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!')
-}) 
+app.use(passport.initialize())
 
 app.listen(3000, () => {
     console.log('Backend executando...')
